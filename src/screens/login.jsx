e2934c523fb5b158fcda72db90a10b6da5eb034c
samina203 <saminabashir203@gmail.com>
2023-03-04 09:25:28 +0500

    login change

--- src/screens/login.jsx
+++ src/screens/login.jsx
@@ -1,15 +1,54 @@
-import { TouchableOpacity, View, Text } from "react-native";
-function Login({ navigation }) {
-  const btnPressed = () => {
-    const sentData = { Name: "ali", age: 24 };
-    navigation.navigate("registersheda", { data: sentData });
-  };
+import { useState } from "react";
+import {
+  TouchableOpacity,
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+  Button,
+} from "react-native";
+function Login(navigation) {
+  const [emailInput, setEmailInput] = useState();
+  const [passwordInput, setPasswordInput] = useState();
+  const btnPressed = () => alert(emailInput);
   return (
-    <View style={{ backgroundColor: "blue", flex: 1 }}>
-      <TouchableOpacity onPress={btnPressed}>
-        <Text>Press</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <View style={styles.loginForm}>
+        <TextInput
+          style={styles.inputBox}
+          placeholder="email"
+          onChangeText={(text) => setEmailInput(text)}
+        ></TextInput>
+        <TextInput
+          placeholder="Password"
+          style={styles.inputBox}
+          onChangeText={(text) => setPasswordInput(text)}
+        ></TextInput>
+      </View>
+      <View style={styles.bottomBox}>
+        <Button title="Loginme" onPress={btnPressed}></Button>
+      </View>
     </View>
   );
 }
 export { Login };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  loginForm: {
+    flex: 1,
+  },
+  bottomBox: {
+    flex: 1,
+    backgroundColor: "purple",
+  },
+  inputBox: {
+    marginVertical: 10,
+    borderRadius: 10,
+    padding: 10,
+    borderWidth: 1,
+  },
+});
